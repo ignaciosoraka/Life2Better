447e8b5aca2c656e3b5c0dac7cd09a3dede012b9
ignaciosoraka <ignaciosoraka@gmail.com>
2024-01-04 09:55:05 -0300

    formulario

--- correos.php
+++ correos.php
@@ -3,28 +3,37 @@ $destinatario = 'ignaciosoraka@gmail.com';
 
 $nombre = $_POST['nombre'];
 $email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
-$ciudad = $_POST['ciudad'];
+$provincia = $_POST['provincia'];
 $mensaje = $_POST['mensaje'];
 
-// Obtener las opciones seleccionadas de la lista de checkboxes
-$menu913 = isset($_POST['menu-913']) ? $_POST['menu-913'] : [];
-$menu913_text = implode(", ", $menu913);  // Convertir array a cadena separada por comas
+// Obtener las opciones seleccionadas de las listas de selección
+$intereses = isset($_POST['Intereses']) ? $_POST['Intereses'] : '';
+$ocupacion = isset($_POST['Ocupacion']) ? $_POST['Ocupacion'] : '';
+$comoNosConociste = isset($_POST['ComoNosConociste']) ? $_POST['ComoNosConociste'] : '';
 
 $mensajeCompleto = "Mensaje de contacto:\n\n";
 $mensajeCompleto .= "Nombre: " . $nombre . "\n";
 $mensajeCompleto .= "Correo Electrónico: " . $email . "\n";
-$mensajeCompleto .= "Ciudad: " . $ciudad . "\n";
+$mensajeCompleto .= "Provincia: " . $provincia . "\n";
 
-// Incluir opciones seleccionadas de la lista de checkboxes
-if (!empty($menu913)) {
-    $mensajeCompleto .= "A qué te dedicas: " . $menu913_text . "\n";
+// Incluir opciones seleccionadas de las listas de selección
+if (!empty($intereses)) {
+    $mensajeCompleto .= "¿Qué estás buscando?: " . $intereses . "\n";
+}
+
+if (!empty($ocupacion)) {
+    $mensajeCompleto .= "¿A qué te dedicas?: " . $ocupacion . "\n";
+}
+
+if (!empty($comoNosConociste)) {
+    $mensajeCompleto .= "¿Cómo nos conociste?: " . $comoNosConociste . "\n";
 }
 
 $mensajeCompleto .= "Mensaje: " . $mensaje . "\n";
 
 $asuntoCorreo = "Consulta de " . $nombre;
 
-$header = "From: AITUE <" . $email . ">\r\n";
+$header = "From: Life2Better <" . $email . ">\r\n";
 $header .= "Reply-To: " . $email . "\r\n";
 $header .= "MIME-Version: 1.0\r\n";
 $header .= "Content-Type: text/plain; charset=UTF-8\r\n";
@@ -39,4 +48,5 @@ if (mail($destinatario, $asuntoCorreo, $mensajeCompleto, $header)) {
     <?php
 }
 ?>
+
 
